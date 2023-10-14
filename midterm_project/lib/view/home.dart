bad39804e9b4529a1f90aa355ad5b2330cb04ad5
MASEOKJAE <maasj7514@gmail.com>
2023-10-14 16:32:03 +0900

    Home 2.0

--- midterm_project/lib/view/home.dart
+++ midterm_project/lib/view/home.dart
@@ -25,102 +25,165 @@ class _HomePageState extends State<HomePage> {
     // products 리스트의 각 요소에 대해 map() 함수를 실행
     return hotels.map((hotel) {
       // 각 product에 대해 아래의 코드 블록을 실행
-      return _isGridView
-          ?
-          // GridView card layout
-          Card(
-              // clipBehavior 속성은 카드의 경계를 넘어가는 자식 위젯들이 어떻게 잘릴지 결정
-              //  Clip.antiAlias는 경계를 부드럽게 만들어 줌
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                // crossAxisAlignment 속성은 수직(열) 방향으로 어떻게 정렬할지 결정 (start -> 시작 위치에서 정렬)
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  // AspectRatio: 특정 비율(aspect ratio)을 유지하면서 그 안에 다른 위젯(여기서는 이미지)를 배치할 때 사용
-                  AspectRatio(
-                    aspectRatio: 18 / 11,
+      if (_isGridView) {
+        // GridView card layout
+        return Card(
+          // clipBehavior 속성은 카드의 경계를 넘어가는 자식 위젯들이 어떻게 잘릴지 결정
+          //  Clip.antiAlias는 경계를 부드럽게 만들어 줌
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            // crossAxisAlignment 속성은 수직(열) 방향으로 어떻게 정렬할지 결정 (start -> 시작 위치에서 정렬)
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // AspectRatio: 특정 비율(aspect ratio)을 유지하면서 그 안에 다른 위젯(여기서는 이미지)를 배치할 때 사용
+              AspectRatio(
+                aspectRatio: 18 / 11,
+                child: Image.asset(
+                  hotel.picture,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            size: 17,
+                            color: Colors.blue,
+                          ),
+                          const SizedBox(width: 5),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    // Hotel star rating with outlined stars
+                                    Row(
+                                      children: List<Widget>.generate(
+                                          hotel.star,
+                                          (index) => const Icon(Icons.star,
+                                              color: Colors.yellow, size: 15.0)),
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                    ),
+                                    const SizedBox(height: 2.0),
+
+                                    Text(
+                                      hotel.name,
+                                      style: const TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold),
+                                      maxLines: 1,
+                                    ),
+                                    const SizedBox(height: 2.0),
+                                    Text(
+                                      hotel.location,
+                                      style: const TextStyle(fontSize: 10),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(
+                            context,
+                            '/detail',
+                            arguments: hotel,
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 5, vertical: 2),
+                          minimumSize: Size.zero,
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        ),
+                        child: const Text('more'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      } else {
+        // ListView card layout
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(3.0, 8.0, 3.0, 8.0),
+            child: Stack(
+              children: [
+                ListTile(
+                  leading: AspectRatio(
+                    aspectRatio: 3 / 2,
                     child: Image.asset(
                       hotel.picture,
                       fit: BoxFit.cover,
                     ),
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Stack(
-                        alignment: Alignment.bottomRight,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              const Icon(
-                                Icons.location_on,
-                                size: 17,
-                                color: Colors.blue,
-                              ),
-                              const SizedBox(width: 5),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children:<Widget>[
-                                        // Hotel star rating with outlined stars
-                                          Row(children:
-                                            List<Widget>.generate(hotel.star, (index) => const Icon(Icons.star, color: Colors.yellow, size: 15.0)),
-                                            mainAxisAlignment : MainAxisAlignment.start,
-                                          ),
-                                          const SizedBox(height: 2.0),
-
-
-                                          Text(hotel.name,
-                                              style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold), 
-                                              maxLines : 1,
-                                          ),
-                                          const SizedBox(height: 2.0),                                      
-                                          Text(hotel.location,
-                                            style : const TextStyle(fontSize: 10),
-                                          ),
-                                      ],
-                                    ),
-                                  ],
-                                )
-                              )
-
-                            ],
-                          ),
-                          TextButton(
-                            onPressed : () {
-                              Navigator.pushNamed(
-                                context,
-                                '/detail',
-                                arguments: hotel,
-                              );
-                            }, 
-                            style: TextButton.styleFrom(
-                              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
-                              minimumSize: Size.zero,
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                            ),
-                            child: const Text('more'),
-                          )
-                        ],
+                  // title: Text(hotel.name),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: List<Widget>.generate(
+                          hotel.star,
+                          (index) => const Icon(Icons.star,
+                              color: Colors.yellow, size: 15.0),
+                        ),
+                        mainAxisAlignment: MainAxisAlignment.start,
                       ),
-                    ),
+                      const SizedBox(height: 2.0),
+                      Text(
+                        hotel.name,
+                        style: const TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.bold),
+                        maxLines: 1,
+                      ),
+                      const SizedBox(height: 2.0),
+                      Text(
+                        hotel.location,
+                        style: const TextStyle(fontSize: 10),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            )
-          :
-          // ListView card layout
-          ListTile(
-              leading: Image.asset(
-                hotel.picture,
-                fit: BoxFit.cover),
-              title: Text(hotel.name),
-              subtitle: Text(hotel.location),
-            );
+                ),
+                Positioned(
+                    // Positioned 위젯은 Stack 내에서 절대 위치를 지정할 수 있습니다.
+                    right: 2, // 오른쪽으로부터의 거리
+                    bottom: 2, // 아래로부터의 거리
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/detail', arguments: hotel);
+                      },
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(
+                                horizontal: (5), vertical: (2))),
+                        minimumSize: MaterialStateProperty.all(Size.zero),
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      ),
+                      child: const Text(
+                        'more',
+                        style: TextStyle(fontSize: 10)),
+                    ))
+              ],
+            ),
+          ),
+        );
+      }
     }).toList();
   }
 
@@ -190,7 +253,6 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Colors.blue,
         iconTheme: const IconThemeData(color: Colors.white),
-
         actions: <Widget>[
           IconButton(
             icon: const Icon(
--- midterm_project/lib/view/search.dart
+++ midterm_project/lib/view/search.dart
@@ -32,109 +32,122 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   void _showSelectedOptions() {
-   showDialog<String>(
+    showDialog<String>(
       context: context,
       builder: (BuildContext context) => AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20), // Optionally, add rounded corners to the dialog.
-          ),
-          title: Container(
-            height: MediaQuery.of(context).size.height / 6, // Set the height to be a third of the screen's height.
-            color: Colors.blue,
-            child: const Center(
-              child: 
-                Text(
-                  'Please check\nyour choice :)', 
-                  style: TextStyle(color: Colors.white),
-                  )
-              ),
-          ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min, // Make the column's height fit its content.
-            children:[
-              ListTile(
-                leading: const Icon(Icons.filter_list, color: Colors.blue), // Add a filter icon before the filters text.
-                title: Text(
-                  '${selectedFilters.join(' / ')}',
-                  style: const TextStyle(fontSize: 12)
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.calendar_month, color: Colors.blue), // Add a calendar icon before the date text.
-                 title: Text(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+              20), // Optionally, add rounded corners to the dialog.
+        ),
+        title: Container(
+          height: MediaQuery.of(context).size.height /
+              6, // Set the height to be a third of the screen's height.
+          color: Colors.blue,
+          child: const Center(
+              child: Text(
+            'Please check\nyour choice :)',
+            style: TextStyle(color: Colors.white),
+          )),
+        ),
+        content: Column(
+          mainAxisSize:
+              MainAxisSize.min, // Make the column's height fit its content.
+          children: [
+            ListTile(
+              leading: const Icon(Icons.filter_list,
+                  color: Colors
+                      .blue), // Add a filter icon before the filters text.
+              title: Text('${selectedFilters.join(' / ')}',
+                  style: const TextStyle(fontSize: 12)),
+            ),
+            ListTile(
+              leading: const Icon(Icons.calendar_month,
+                  color:
+                      Colors.blue), // Add a calendar icon before the date text.
+              title: Text(
                   'IN   ${checkInDate.toLocal().toString().split(' ')[0]}',
-                   style: const TextStyle(fontSize: 12)
-                  ),  
-              )
+                  style: const TextStyle(fontSize: 12)),
+            )
           ],
         ),
-        actions:<Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {Navigator.pop(context,'Search');},
-                  child: const Text('Search'),
-                  style: ElevatedButton.styleFrom(
+        actions: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, 'Search');
+                },
+                child: const Text('Search'),
+                style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)
-                    )
-                  )
-                ),
-                const SizedBox(width:10),
-
-                ElevatedButton(
-                  onPressed: () {Navigator.pop(context, 'Cancel');},
-                  child: const Text('Cancel'),
-                  style: ElevatedButton.styleFrom(
-                      shape:RoundedRectangleBorder(
-                        borderRadius:BorderRadius.circular(10.0)
-                      )
-                    )
-                ),
-            ]
-          )
+                        borderRadius: BorderRadius.circular(10.0)))),
+            const SizedBox(width: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, 'Cancel');
+                },
+                child: const Text('Cancel'),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)))),
+          ])
         ],
       ),
     );
   }
 
-   void onFilterChanged(List<String> filters) {
-     setState(() {
-       selectedFilters = filters;
-     });
-   }
+  void onFilterChanged(List<String> filters) {
+    setState(() {
+      selectedFilters = filters;
+    });
+  }
 
-   @override
-   Widget build(BuildContext context) {
-
-     return Scaffold(
-       appBar: AppBar(title: const Text('Search')),
-       body:
-         ListView(
-          children:[
-            ExpansionView(onFilterChanged: handleFilterChange),
-            ListTile(
-              title: OutlinedButton(
-                onPressed : () => _selectDate(context),
-                  child : const Text('Select Check-in date')
-                  ),
-              ),
-            Center( // Wrap the button with a Center widget to center it horizontally
-              child: Padding( // Add some vertical padding
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: MaterialButton(
-                  onPressed : _showSelectedOptions,
-                  color : Theme.of(context).primaryColor, // Use the primary color of your theme for the button's background
-                  textColor : Colors.white, // Set the text color to white
-                  shape : RoundedRectangleBorder(borderRadius : BorderRadius.circular(10)), // Make the button's corners rounded
-                  child : const Text('Search'),
-                  minWidth : 3, // Remove minimum width restriction 
-                  padding : const EdgeInsets.symmetric(horizontal: 8, vertical: 8), // Adjust padding as per requirement
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Search')),
+      body: ListView(
+        children: [
+          ExpansionView(onFilterChanged: handleFilterChange),
+          ListTile(
+            title: OutlinedButton(
+                onPressed: () => _selectDate(context),
+                child: const Text('Select Check-in date')),
+          ),
+          // Positioned(
+          //   left: 0,
+          //   right: 0,
+          //   bottom: 0,
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(10.0),
+          //     child: MaterialButton(
+          //       onPressed: _showSelectedOptions,
+          //       color: Theme.of(context).primaryColor,
+          //       textColor: Colors.white,
+          //       shape: RoundedRectangleBorder(
+          //         borderRadius: BorderRadius.circular(10),
+          //       ),
+          //       child: Text('Search', style: TextStyle(fontSize: (20))),
+          //       minWidth: (3),
+          //       padding: const EdgeInsets.symmetric(horizontal: (12), vertical: (12)),
+          //     ),
+          //   ),
+          // )
+        ],
+      ),
+      floatingActionButton: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: MaterialButton(
+                onPressed: _showSelectedOptions,
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
+                child: const Text('Search', style: TextStyle(fontSize: (20))),
+                // minWidth: (3),
+                padding: const EdgeInsets.symmetric(horizontal: (20), vertical: (12)),
               ),
-            )
-         ])
-     );
-   }
+            ),
+    );
+  }
 }
